e594050cb1157080efec9930d7a44d1503b4348c
neha-decrypt <neha.decrypt@gmail.com>
2022-05-27 11:12:17 +0530

    fixed issue

--- app/routers/utils/index.js
+++ app/routers/utils/index.js
@@ -2,11 +2,15 @@ const router = require("express").Router();
 const utilsController = require("./lib/controllers");
 const utilsMiddleware = require("./lib/middleware");
 
-router.post("/addCategory", utilsMiddleware.verifyToken, utilsController.addCategory);
+router.post(
+  "/addCategory",
+  utilsMiddleware.verifyToken,
+  utilsController.addCategory
+);
 
 router.post("/addBrand", utilsMiddleware.verifyToken, utilsController.addBrand);
 
-router.get("/getAllCategory", utilsMiddleware.verifyToken, utilsController.getAllCategory);
-router.get("/getAllBrand", utilsMiddleware.verifyToken, utilsController.getAllBrand);
+router.get("/getAllCategory", utilsController.getAllCategory);
+router.get("/getAllBrand", utilsController.getAllBrand);
 
 module.exports = router;
